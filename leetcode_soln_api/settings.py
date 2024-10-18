7c41fb9b2f70606f239abf07609f62d709a86133
cjspraneeth <91653674+cjspraneeth@users.noreply.github.com>
2024-10-18 13:23:15 +0000

    restructure the files

--- leetcode_soln_api/settings.py
+++ leetcode_soln_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
 import os
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR.parent, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
   # Reads the .env file
 
